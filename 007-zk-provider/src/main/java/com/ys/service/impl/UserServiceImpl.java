e07e3feb5acd232d85c076bcd562db3bc52a6d9c
apple <478767276@qq.com>
2020-12-04 18:41:54 +0800

    commit

--- 007-zk-provider/src/main/java/com/ys/service/impl/UserServiceImpl.java
+++ 007-zk-provider/src/main/java/com/ys/service/impl/UserServiceImpl.java
@@ -2,9 +2,7 @@ package com.ys.service.impl;
 
 import com.ys.domain.User;
 import com.ys.service.UserService;
-import org.springframework.stereotype.Service;
 
-@Service
 public class UserServiceImpl implements UserService {
     @Override
     public User queryUserById(Integer id,String userName) {
